963e69b74cb37d32f9b9730782a8943cf4bd36ec
Kam Lotfull <kamlotfull@gmail.com>
2019-11-25 22:43:18 +0300

    Adds `update_nums_df` call at `app.ready` hook and periodic timeloop job

--- rossvyaz/apps.py
+++ rossvyaz/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class RossvyazConfig(AppConfig):
     name = 'rossvyaz'
+
+    def ready(self):
+        from . import info
+        info.update_nums_df()
---
+++ rossvyaz/timeloop.py
@@ -0,0 +1,11 @@
+from datetime import timedelta
+
+from timeloop import Timeloop
+
+tl = Timeloop()
+
+
+@tl.job(interval=timedelta(days=1))
+def update_job():
+    from . import info
+    info.update_nums_df()
