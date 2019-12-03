bf2be86e16de79f9ce7374c53ad94dadc18e02b9
Kam Lotfull <kamlotfull@gmail.com>
2019-12-03 18:23:53 +0300

    Adds `tests.py`

--- rossvyaz/tests.py
+++ rossvyaz/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
 
-# Create your tests here.
+from rossvyaz.models import Phone, Operator, Region
+
+
+class PhoneTest(TestCase):
+    @staticmethod
+    def create_phone(begin='9000000000', end='9001000000', operator='Operator', region='Region'):
+        o, created = Operator.objects.get_or_create(name=operator)
+        r, created = Region.objects.get_or_create(name=region)
+        return Phone.objects.create(begin=begin, end=end, operator=o, region=r)
+
+    def test_phone_validation_ok(self):
+        pass
+
+    def test_phone_validation_fail(self):
+        pass
+
+    def test_phone_find_found(self):
+        expected = self.create_phone()
+        number_to_find = '9000123456'
+        phone = Phone.find(number_to_find)
+        self.assertEqual(expected, phone)
+
+    def test_phone_find_fail(self):
+        lower = self.create_phone()
+        greater = self.create_phone(begin='9990000000', end='9999999999')
+        number_to_fail = '9856660205'
+        phone = Phone.find(number_to_fail)
+        self.assertTrue(phone is None)
+
+    def test_data_loading(self):
+        pass
